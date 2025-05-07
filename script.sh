303a455136a7ef843c6da5f8b5e6b233a422e52e
anhenrique <anhenrique1@gmail.com>
2025-05-07 15:51:55 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,9 +4,25 @@ echo '================================Atualizando Kernel========================
 sudo apt update
 sudo apt install -y --install-recommends linux-generic-hwe-20.04
 
+echo '================================Desinstalar Kaspersky ====================================='
+sudo systemctl  stop kesl klnagent64
+
+sudo apt remove -y klnagent64 kesl
+
+sudo rm -rf /opt/kaspersky
+sudo rm -rf /var/opt/kaspersky
+sudo rm -rf /etc/opt/kaspersky
+sudo rm -rf /var/log/kaspersky
+
 echo '===============================Instalando Kaspersky===================================='
 SESSION_NAME="kaspersky"
+
 tmux new-session -d -s $SESSION_NAME # kaspersky
+
+tmux send-keys -t $SESSION_NAME "chmod +x /tmp/klnagent64_13.2.2-1263_amd64.deb" Enter
+sleep 3
+tmux send-keys "dpkg -i /tmp/klnagent64_13.2.2-1263_amd64.deb" Enter
+sleep 10
 tmux send-keys -t $SESSION_NAME "cd /opt/kaspersky/klnagent64/lib/bin/setup" Enter
 sleep 2
 tmux send-keys "./postinstall.pl" Enter
